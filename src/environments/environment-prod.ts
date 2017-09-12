b1230a2f32e02f58035050577c2a74ed0e00d545
CuisinierClement <clement.cuisinier@yahoo.fr>
2017-09-12 16:01:26 +0200

    commit new install angular + config angular avec firebase

---
+++ src/environments/environment-prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        // same as above, or use a different firebase project to isolate environments
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyAMYqU49_ADxaZDUMPtPg58P7-NunCG9NU',
+        authDomain: 'momento-9318a.firebaseapp.com',
+        databaseURL: 'https://momento-9318a.firebaseio.com',
+        projectId: 'momento-9318a',
+		storageBucket: 'momento-9318a.appspot.com',
+		messagingSenderId: '666727240525'
+    }
+};
