8ddd8d701e33bafab2343bcd11289eb9632d3b17
kido-san <adam.presta@gmail.com>
2017-09-14 11:10:25 +0200

    New Css and dependencies added, single component added

---
+++ src/app/items/item-single/item-single.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {ItemService} from '../shared/item.service';
+import {Item} from '../shared/item';
+import {FirebaseObjectObservable} from "angularfire2/database";
+import {Router, ActivatedRoute, Params} from "@angular/router";
+@Component({
+  selector: 'item-single',
+  templateUrl: './item-single.component.html',
+  styleUrls: ['./item-single.component.css']
+})
+export class ItemSingleComponent implements OnInit {
+
+  item: FirebaseObjectObservable<Item>;
+  itemId: string;
+  showSpinner = true;
+
+  constructor(private itemSvc: ItemService, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.itemId = params['id'];
+      console.log(this.itemId);
+    });
+
+    this.item = this.itemSvc.getItem(this.itemId);
+    this.item.subscribe(() => this.showSpinner = false);
+    console.log(this.item);
+  }
+
+}
